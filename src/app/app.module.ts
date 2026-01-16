9a0b849e0a3642f317180d4f0195ff3a8f61004c
dhanashri-bhopale <bhopaledhanashri@gmail.com>
2026-01-16 16:58:13 +0530

    todoIO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { TodoDashboardComponent } from './shared/components/todo-dashboard/todo-dashboard.component';
+import { TodoListComponent } from './shared/components/todo-list/todo-list.component';
+import { TodoFormComponent } from './shared/components/todo-form/todo-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoDashboardComponent,
+    TodoListComponent,
+    TodoFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
